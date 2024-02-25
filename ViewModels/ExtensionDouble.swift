030d07bdc4ca1cc929e5c1cbf0eea4d08dc3b397
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-25 18:04:46 +0100

    ExtensionDouble

---
+++ ViewModels/ExtensionDouble.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension Double {
+    func asString(style: DateComponentsFormatter.UnitsStyle) -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second, .nanosecond]
+        formatter.unitsStyle = style
+        return formatter.string(from: self) ?? ""
+     }
+}
