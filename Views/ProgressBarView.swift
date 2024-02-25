cd0b97c470df532f2c112c6285c1b6b8bfecfecf
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-25 19:01:30 +0100

    progressCircle UI

--- Views/ProgressBarView.swift
+++ Views/ProgressBarView.swift
@@ -14,13 +14,28 @@ struct ProgressBarView: View {
     
     // MARK: - View Body
     var body: some View {
-        defaultCircle
+        ZStack {
+            // Default Circle
+            defaultCircle
+            // Overlap Circle
+            progressCircle
+        }
     }
     
     private var defaultCircle: some View {
         Circle()
             .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .butt, dash: [2, 6]))
             .fill(.gray)
+            .rotationEffect(Angle(degrees: -90))
+            .frame(width: 300, height: 300)
+    }
+    
+    private var progressCircle: some View {
+        Circle()
+            .trim(from: 0.0, to: CGFloat(progress) / CGFloat(goal))
+            .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .butt, dash: [2, 6]))
+            .fill(Color(red: 236/255, green: 230/255, blue: 0/255))
+            .animation(.spring(), value: progress)
             .rotationEffect(Angle(degrees: -90))
             .frame(width: 300, height: 300)
     }
