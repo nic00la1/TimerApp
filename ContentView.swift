ab702cab32338b8ee131365f14d001aa7010092c
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-25 19:08:55 +0100

    properties and backgroundColor

--- ContentView.swift
+++ ContentView.swift
@@ -8,15 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    // MARK: - Properties
+    @ObservedObject var timerViewModel : TimerViewModel
+    @State var isPause = false
+    @State private var rotation = 0
+    
+    // MARK: - Initializer
+    init(seconds: TimeInterval = 0) {
+        timerViewModel = TimerViewModel(seconds: seconds, goalTime: 20)
     }
+    
+    // MARK: - View Body
+    var body: some View {
+        ZStack {
+            backgroundColor
+        }
+    }
+    
+    // MARK: - Private Views
+    private var backgroundColor: some View {
+        Color(red: 63/255, green: 68/255, blue: 3/55)
+            .ignoresSafeArea()
+    }
+    
 }
 
 #Preview {
