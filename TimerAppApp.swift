67234713c703da13a76adb6b5f80368729382cb7
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-25 17:41:49 +0100

    Initial commit

---
+++ TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Nicola Kaleta on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
