ca5cbbef61d19e756c2e6b67df98adce23df0cb9
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-25 17:51:14 +0100

    initializer and startup for timer methods

--- ViewModels/TimerViewModel.swift
+++ ViewModels/TimerViewModel.swift
@@ -21,4 +21,22 @@ class TimerViewModel : NSObject, ObservableObject {
     private var soundID: SystemSoundID = 1407 // id for playing the sound
     private var feedback =  UIImpactFeedbackGenerator(style: .soft)
     
+    // MARK: - Initializer
+    init(seconds: TimeInterval, goalTime: Double) {
+        self.seconds = seconds
+        self.goalTime = goalTime
+    }
+    
+    // MARK: - Timer Methods
+    @objc func fireTimer(){
+        seconds += 0.2
+        progress = Double(seconds) / Double(goalTime)
+        self.displayTime = calculateDisplayTime()
+        
+        // Timer is completed
+        if progress >= 1 {
+            stopSession()
+            makeSoundAndVibration()
+        }
+    }
 }
